7b3f7727d379bb361ad11a76270130ebd61360ef
DaWe <10603822+DaWe35@users.noreply.github.com>
2020-04-03 22:27:58 +0200

    just some small improvements

--- appointment.js
+++ appointment.js
@@ -1,18 +1,39 @@
+document.addEventListener('DOMContentLoaded', function () {
+	document.body.innerHTML += `
+		<div class="call-now-container">
+		<div class="call-now-card">
+			<h4>Call Now!</h4>
+			<p>We are the best company!</p>
+			<a href="#" class="num"><i class="glyphicon glyphicon-earphone"></i>+11 (30) 123 1234</a>
+		</div>
+		
+		<div class="button-container">
+			<button type="button" class="call-now-appointment ring" onclick="collapse()">
+				<div class="content">
+					<i class="glyphicon glyphicon-earphone"></i> <span class="button-text">Book Appointment!</span>
+				</div>
+				<div class="cross">
+					<i class="glyphicon glyphicon-remove"></i>
+				</div>
+			</button>
+		</div>
+		</div>`
+});	
+
 function collapse () {
-    const bouton = document.getElementsByClassName('appointment')[0];
-    const carte = document.getElementsByClassName('card')[0];
+    const bouton = document.getElementsByClassName('call-now-appointment')[0];
+    const carte = document.getElementsByClassName('call-now-card')[0];
     
     if (bouton.style.width == '1.5em') {
-      bouton.style.width = '100%';
+      bouton.style.width = '220px';
       bouton.style.height = '1.5em';
       bouton.style.borderRadius = '100px';
-      bouton.style.padding = '15px 0 15px 0';
+      bouton.style.padding = '10px 20px';
       bouton.childNodes[1].style.opacity = '1';
       bouton.childNodes[3].style.opacity = '0';
       carte.style.opacity = '0';      
       carte.style.pointerEvents = 'none';
-    }
-    else {
+    } else {
       try {
         clearInterval(boucle);
       }
@@ -30,11 +51,10 @@ function collapse () {
 }
 
 function anim() {
-  const e = document.getElementsByClassName('appointment')[0];  
+  const e = document.getElementsByClassName('call-now-appointment')[0];  
   e.classList.remove('ring');
   void e.offsetWidth;
   e.classList.add('ring');
 }
 
 boucle = setInterval(anim, 3000);
-boucle();
